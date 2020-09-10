5b3c92d4c278c9862b862b5e5abdcaf2e4e2900b
frillic <69489498+crowe828@users.noreply.github.com>
2020-09-09 22:19:57 -0400

    set up variable to only let you select passwords of the required length

--- script.js
+++ script.js
@@ -5,38 +5,46 @@
 
 // Application prompts user for password length and if they would like to include lowercased, uppercased, numeric, and special characters
 // Application generates valid password based on provided options
-// Application requires minimum password length of 8 characters
-// Application requires maximum password length of 128 characters
 
+
+// Code that makes the "generate" button function
 var generateBtn = document.querySelector("#generate");
-// Array for numbers
+
 // Array for letters (uppercased and lowercased)
 // Array for special digits
-
-// Set it so that only numbers entered are valid between 8 and 128, including both.
-
-// Set a prompt asking if they would like lowercased letters in their password
-
-// Set a prompt asking if they would like uppercased letters in their password
-
-// Set a prompt asking if they would like numbers in their password
-
-// Set a prompt asking if they would like special characters in their password (and show what the special characters are)
-
-// Prompts must each continue to the next prompt regardless of choice
-
-// The code must take all of these variables and create a random password, meeting all of these requirements. Only whole numbers.
-
-// set a "return" of the final output that will display the newly generated password
 
 function generatePassword() {
   
   // Password cannot be less than 8 or exceed 128 characters total
-  var numberCharacters = prompt("Enter number of characters. Must be between 8 and 128 characters.")
+  var numPrompt = prompt("Enter number of characters. Must be between 8 and 128 characters.");
 
-  if
+  // If password is within 8 and 128 characters long, you may proceed
+  if (numPrompt >= 8 && numPrompt <= 128) {
+    alert("Your password will be " + numPrompt + " characters long.")
+  }
+
+  // If password is not the correct length you will receive this message
+  else {
+    alert("Please select a corret password length.");
+  }
+  // Set it so that only numbers entered are valid between 8 and 128, including both.
+
+  // Set a prompt asking if they would like lowercased letters in their password
+
+  // Set a prompt asking if they would like uppercased letters in their password
+
+  // Set a prompt asking if they would like numbers in their password
+
+  // Set a prompt asking if they would like special characters in their password (and show what the special characters are)
+
+  // Prompts must each continue to the next prompt regardless of choice
+
+  // The code must take all of these variables and create a random password, meeting all of these requirements. Only whole numbers.
+
+  // set a "return" of the final output that will display the newly generated password 
+
   //return for the test var, will be changed.
-  return numberCharacters
+  return numPrompt;
 }
 
 // Write password to the #password input

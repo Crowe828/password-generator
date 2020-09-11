e6fea1433310bfa9a2fa29c2ce1a160e058f9d25
frillic <69489498+crowe828@users.noreply.github.com>
2020-09-10 20:54:35 -0400

    initial generator is complete

--- script.js
+++ script.js
@@ -1,84 +1,77 @@
 // Code that makes the "generate" button function
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate")
 
 // Array of lowercase letters
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 // Array of UPPERCASE letters
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 // Array of special characters
-var spCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`", "{", "|", "}", "~"]
+var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "`", "{", "|", "}", "~"]
 
 // Array of digits 0-9
 var nums = [0,1,2,3,4,5,6,7,8,9]
+  
+var total = []
+
+var pwd = []
 
 // Function which runs the password generator
 function generatePassword() {
   
   // Password cannot be less than 8 or exceed 128 characters total
   var numCount = prompt("Enter number of characters. Must be between 8 and 128 characters.");
+  console.log(numCount); 
 
   // If password is within 8 and 128 characters long, user may proceed
   if (numCount >= 8 && numCount <= 128) {
     
-    // An alert letting you know how many characters you selected
-    alert("Your password will be " + numCount + " characters long.");
-
     // Does the user want to include lowercase letters in their password?
     var lowerPrompt = confirm("Would you like to include lowercase letters?");
     
-    //Whether the user selects ok or cancel it will move on to the next message
-    if (lowerPrompt = true || false) {
-
-      // Does the user want to include UPPERCASE letters in their password?
-      var upperPrompt = confirm("Would you like to include UPPERCASE letters?");
+    if (lowerPrompt) {
+      
+      total.push(...lower);
+      console.log(total);
     }
 
-    //Whether the user selects ok or cancel it will move on to the next message
-    if (upperPrompt = true || false) {
+    // Does the user want to include UPPERCASE letters in their password?
+    var upperPrompt = confirm("Would you like to include UPPERCASE letters?");
 
-      // Does the user want to include numbers in their password?
-      var numPrompt  = confirm("Would you like to include numbers?");
+    if (upperPrompt) {
+      
+      total.push(...upper);
+      console.log(total);
     }
+    
+    // Does the user want to include numbers in their password?
+    var numPrompt  = confirm("Would you like to include numbers?");
 
-    //Whether the user selects ok or cancel it will move on to the next message
-    if (numPrompt = true || false) {
+    if (numPrompt) {
+      
+      total.push(...nums);
+      console.log(total);
+    }
 
     // Does the user want to include special characters in their password?
-    var spPrompt = confirm("Would you like to include special characters?");
+    var specialPrompt = confirm("Would you like to include special characters?");
+    
+    if (specialPrompt) {
+      
+      total.push(...special);
+      console.log(total);
+    }
+    
+    for (var i = 0; i < numCount; i++) {
+      pwd.push(total[Math.floor(Math.random() * total.length)]);
     }
 
-    //Whether the user selects ok or cancel it will move on to the next message
-    if (spPrompt = true || false) {
-      
-      // Password was able to be generated
-      alert("Password successfully generated!");
-    }
-    
-    // Provides a random lowercase letter
-    var lowerRand = lowerCase[Math.floor(Math.random() * lowerCase.length)];
-        
-    // Provides a random uppercase letter
-    var upperRand = upperCase[Math.floor(Math.random() * upperCase.length)];
-    
-    // Provides a random special character
-    var spRand = spCharacters[Math.floor(Math.random() * spCharacters.length)];
-              
-    // Provides a random number 0-9
-    var numRand = nums[Math.floor(Math.random() * nums.length)];
-        
-    // Concat putting together all 4 arrays
-    var pwd = lowerRand.concat(upperRand, spRand, numRand);
   }
   
-  // If you enter an incorrect length for the password you will receive this message
-  else {
-    alert("Please press ''Generate Password'' to start over.");
-  }
+  // Displays the password
+  return pwd.join("")
   
-  //return for the test var, will be changed.
-  return (pwd);
 }
 
 // Write password to the #password input
